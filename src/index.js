9f7ce86df8b356a0f4d3606371bf2532b7770fa0
Aliaksandr_Zahorski <aliaksandr_zahorski@epam.com>
2017-12-21 13:16:26 +0300

    solution is added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function findRotationPoint(words) {
-  // your solution here
+  const low = 0;
+  const high = words.length;
+  const endOfArray = words.length - 1;
+
+  const findPivot = (arr, low, high) => {
+    let mid = Math.floor((low + high) / 2);
+
+    if (mid === high || mid === low) {
+      if (mid === endOfArray) {
+        return 0;
+      }
+      return mid + 1;
+    }
+
+    if (arr[low] > arr[mid]) {
+      return findPivot(arr, low, mid);
+    }
+
+    return findPivot(arr, mid, high);
+  }
+
+  return findPivot(words, low, high);
 }
